d1d893f8cd9262deda296ad18b6270aec596c1c7
SASI2SITE <165626716+SASI2SITE@users.noreply.github.com>
2024-04-15 23:06:14 +0530

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -36,7 +36,7 @@ export default function Register() {
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Don't have account register here with SASI mail id..
+            Regiter here with your SASI mail id..
           </h1>
           <form
             onSubmit={handleSubmit}
@@ -57,7 +57,7 @@ export default function Register() {
                 onChange={(e) => setEmail(e.target.value)}
                 id="email"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@sasi.ac.in"
+                placeholder="name@company.com"
                 required
               />
             </div>
